b28564c3de2b36f46cc51d4247b25677c2b57055
gnishida <gnishida@purdue.edu>
2013-11-17 09:34:31 -0500

    Improve CG.

--- Graphics5/ShaderStencilTextureWithSoftShadowMappingInterface.cpp
+++ Graphics5/ShaderStencilTextureWithSoftShadowMappingInterface.cpp
@@ -50,6 +50,7 @@ void ShaderStencilTextureWithSoftShadowMappingInterface::PerFrameInit() {
 	}
 
 	// copy the vertices data
+	/*
 	float tris[3][15];
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 5; j++) {
@@ -58,32 +59,49 @@ void ShaderStencilTextureWithSoftShadowMappingInterface::PerFrameInit() {
 			tris[i][j * 3 + 2] = (float)scene->meshes[i]->tris[(j + 1) * 6 + 5] + i * 24;
 		}
 	}
+	*/
 
-	float verts[3][24][3];
-	float norms[3][24][3];
-	float texCoords[3][24][2];
+	float verts[3][15][3];
+	float norms[3][5][3];
+	float texCoords[3][15][2];
 	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 24; j++) {
-			verts[i][j][0] = (float)scene->meshes[i]->verts[j].v.x();
-			verts[i][j][1] = (float)scene->meshes[i]->verts[j].v.y();
-			verts[i][j][2] = (float)scene->meshes[i]->verts[j].v.z();
-			norms[i][j][0] = (float)scene->meshes[i]->verts[j].n.x();
-			norms[i][j][1] = (float)scene->meshes[i]->verts[j].n.y();
-			norms[i][j][2] = (float)scene->meshes[i]->verts[j].n.z();
-			texCoords[i][j][0] = (float)scene->meshes[i]->verts[j].t[0];
-			texCoords[i][j][1] = (float)scene->meshes[i]->verts[j].t[1];
+		for (int j = 0; j < 5; j++) {
+			verts[i][j * 3][0] = (float)scene->meshes[i]->verts[(j + 1) * 4].v.x();
+			verts[i][j * 3][1] = (float)scene->meshes[i]->verts[(j + 1) * 4].v.y();
+			verts[i][j * 3][2] = (float)scene->meshes[i]->verts[(j + 1) * 4].v.z();
+
+			verts[i][j * 3 + 1][0] = (float)scene->meshes[i]->verts[(j + 1) * 4 + 1].v.x();
+			verts[i][j * 3 + 1][1] = (float)scene->meshes[i]->verts[(j + 1) * 4 + 1].v.y();
+			verts[i][j * 3 + 1][2] = (float)scene->meshes[i]->verts[(j + 1) * 4 + 1].v.z();
+
+			verts[i][j * 3 + 2][0] = (float)scene->meshes[i]->verts[(j + 1) * 4 + 3].v.x();
+			verts[i][j * 3 + 2][1] = (float)scene->meshes[i]->verts[(j + 1) * 4 + 3].v.y();
+			verts[i][j * 3 + 2][2] = (float)scene->meshes[i]->verts[(j + 1) * 4 + 3].v.z();
+
+			norms[i][j][0] = (float)scene->meshes[i]->verts[(j + 1) * 4].n.x();
+			norms[i][j][1] = (float)scene->meshes[i]->verts[(j + 1) * 4].n.y();
+			norms[i][j][2] = (float)scene->meshes[i]->verts[(j + 1) * 4].n.z();
+
+			texCoords[i][j * 3][0] = (float)scene->meshes[i]->verts[(j + 1) * 4].t[0];
+			texCoords[i][j * 3][1] = (float)scene->meshes[i]->verts[(j + 1) * 4].t[1];
+
+			texCoords[i][j * 3 + 1][0] = (float)scene->meshes[i]->verts[(j + 1) * 4 + 1].t[0];
+			texCoords[i][j * 3 + 1][1] = (float)scene->meshes[i]->verts[(j + 1) * 4 + 1].t[1];
+
+			texCoords[i][j * 3 + 2][0] = (float)scene->meshes[i]->verts[(j + 1) * 4 + 3].t[0];
+			texCoords[i][j * 3 + 2][1] = (float)scene->meshes[i]->verts[(j + 1) * 4 + 3].t[1];
 		}
 	}
 
 	// set parameters for the fragment shader
-	cgGLSetParameterArray1f(fragmentMeshTris, 0, 15 * 3, (float*)tris[0]);
-	cgGLSetParameterArray3f(fragmentMeshVerts, 0, 24 * 3, (float*)verts[0]);
-	cgGLSetParameterArray3f(fragmentMeshNorms, 0, 24 * 3, (float*)norms[0]);
-	cgGLSetParameterArray2f(fragmentMeshTexCoords, 0, 24 * 3, (float*)texCoords[0]);
+	//cgGLSetParameterArray1f(fragmentMeshTris, 0, 15 * 3, (float*)tris[0]);
+	cgGLSetParameterArray3f(fragmentMeshVerts, 0, 15 * 3, (float*)verts[0]);
+	cgGLSetParameterArray3f(fragmentMeshNorms, 0, 5 * 3, (float*)norms[0]);
+	cgGLSetParameterArray2f(fragmentMeshTexCoords, 0, 15 * 3, (float*)texCoords[0]);
 
 	cgGLSetParameter3fv(fragmentLightCenter, (float*)&scene->light->position);
 	cgGLSetParameter1f(fragmentLightStep, (float)scene->light_step);
-	cgGLSetParameter1f(fragmentNumLight, 4.0f);
+	cgGLSetParameter1f(fragmentNumLight, 5.0f);
 
 	cgGLSetParameter3fv(fragmentEyePosition, (float*) &(scene->currentPPC->C));
 	cgGLSetParameter3fv(fragmentKa, (float*) &(scene->light->ambient));
